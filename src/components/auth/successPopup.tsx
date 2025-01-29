f868600d24478b619cde697cebac061d7688af2a
Resvan K <muhammedresvan@gmail.com>
2025-01-29 22:46:52 +0530

    image-optimizatoin

--- src/components/auth/successPopup.tsx
+++ src/components/auth/successPopup.tsx
@@ -12,7 +12,7 @@ export default function SucessPopup({ open, setOpen }: { open: boolean; setOpen:
           <AlertDialogContent className="bg-background/80 px-24 py-14">
               <AlertDialogHeader className="flex flex-col gap-5">
                   <AlertDialogTitle className="flex justify-center items-center">
-                      <Image src={'/images/success-image.png'} alt="success-image" width={265} height={140} />
+                      <Image priority src={'/images/success-image.png'} alt="success-image" width={265} height={140} />
                   </AlertDialogTitle>
                   <AlertDialogTitle className="text-center font-plus-jakarta-sans text-base md:text-2xl font-bold leading-[130%]">
                       Successfully Logged in
