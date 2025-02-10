6b6f327728960988427ae264da2b8a21954b5728
unknown <mubashir.dev369@gmail.com>
2025-02-10 16:46:09 +0530

    explore

--- src/components/expert-card.tsx
+++ src/components/expert-card.tsx
@@ -5,28 +5,44 @@ import Image from 'next/image'
 import { Star } from 'lucide-react'
 import Link from 'next/link'
 
-export default function ExpertCard({ prof }: { prof: { image: string; name: string; price: string; rating: number; bio: string; }}) {
+export default function ExpertCard({
+  prof,
+}: {
+  prof: {
+    profile_image: string;
+    name: string;
+    min_session_price: string;
+    average_rating: number;
+    about_me: string;
+    _id:string
+  };
+}) {
+
   return (
-      <Link href={'/expert/1'} className="relative">
-          <div className="w-full">
-              <AspectRatio ratio={1 / 1}>
-                  <Image
-                      src={prof.image}
-                      alt={prof.name}
-                      fill
-                      className="rounded-lg object-cover"
-                  />
-              </AspectRatio>
-          </div>
-          <p className="text-sm font-bold">{prof.name}</p>
-          <div className="flex justify-between items-center">
-              <p className="text-xs font-semibold">${prof.price}</p>
-              <div className="flex items-center gap-0.5">
-                  <Star className="h-4 w-auto fill-[#FBBC05] text-[#FBBC05]" />
-                  <span className="text-sm">{prof.rating}</span>
-              </div>
-          </div>
-          <p className="text-xs text-muted-foreground line-clamp-3 mt-1">{prof.bio}</p>
-      </Link>
-  )
+    <Link href={`/expert/${prof.name}`} className="relative">
+      <div className="w-full">
+        <AspectRatio ratio={1 / 1}>
+          <Image
+            src={prof.profile_image}
+            alt={prof.name}
+            fill
+            className="rounded-lg object-cover"
+          />
+        </AspectRatio>
+      </div>
+      <p className="text-sm font-bold">{prof.name}</p>
+      <div className="flex justify-between items-center">
+        <p className="text-xs font-semibold">
+          ${prof.min_session_price ? prof.min_session_price : 0.0}
+        </p>
+        <div className="flex items-center gap-0.5">
+          <Star className="h-4 w-auto fill-[#FBBC05] text-[#FBBC05]" />
+          <span className="text-sm">{prof.average_rating}</span>
+        </div>
+      </div>
+      <p className="text-xs text-muted-foreground line-clamp-3 mt-1">
+        {prof.about_me}
+      </p>
+    </Link>
+  );
 }
