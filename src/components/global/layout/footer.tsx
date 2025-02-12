a4b9a1f7a38b7af0264222498858bde945a29411
goformeet <info@goformeet.co>
2025-02-13 01:06:12 +0530

    Update footer ios link update

--- src/components/global/layout/footer.tsx
+++ src/components/global/layout/footer.tsx
@@ -36,7 +36,10 @@ export default function Footer() {
                     </Link>
                     <div className='flex justify-between items-center mt-12'>
                         <Image src={'/images/play-store.svg'} width={135} height={40} alt='Play Store Image' />
-                        <Image src={'/images/app-store.svg'} href={'https://apps.apple.com/us/app/meetxo/id6741767901'} width={135} height={40} alt='App Store Image' />
+                        <a href="https://apps.apple.com/us/app/meetxo/id6741767901" target="_blank" rel="noopener noreferrer">
+  <Image src={'/images/app-store.svg'} width={135} height={40} alt='App Store Image' />
+</a>
+{/*                         <Image src={'/images/app-store.svg'} width={135} height={40} alt='App Store Image' /> */}
                     </div>
                 </div>
             </div>
