d5f9be759c4fbf673114a16c5492d21fd3fc6c9d
Resvan K <muhammedresvan@gmail.com>
2025-02-14 18:39:14 +0530

    updates

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,14 @@
-import { hostname } from "os";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
-    { protocol: "https", hostname: "example.com" },
+      { protocol: "https", hostname: "example.com" },
       { protocol: "https", hostname: "media.licdn.com" },
-<<<<<<< Updated upstream
       { protocol: "https", hostname: "cdn.example.com" },
       { protocol: "https", hostname: "**.cloudinary.com" },
-      { protocol: "https", hostname: "goformeet.s3.ap-south-1.amazonaws.com" }, 
-      { protocol: "https", hostname: "www.goformeet.co" }, 
-=======
-      { protocol: "https", hostname: "cdn.example.com" }, 
-      { protocol: "https", hostname: "res.cloudinary.com" },
-      { protocol: 'https', hostname: "**"}
->>>>>>> Stashed changes
+      { protocol: "https", hostname: "goformeet.s3.ap-south-1.amazonaws.com" },
+      { protocol: "https", hostname: "www.goformeet.co" },
     ],
   },
 };
