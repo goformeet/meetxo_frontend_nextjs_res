f467672bae370f3c24422632b41b6bad8a99b309
goformeet <info@goformeet.co>
2025-02-20 16:25:10 +0530

    Update connect-with-top-experts-section.tsx

--- src/components/home/connect-with-top-experts-section.tsx
+++ src/components/home/connect-with-top-experts-section.tsx
@@ -14,7 +14,9 @@ export default function ConnectWithTopExpertsSection() {
     const items = [
         'Find an Expert',
         'Book a Video Call',
-        'Virtual Consultation'
+        'Virtual Consultation',
+            "Offline Meetings",
+
     ];
     return (
         <section className="py-14 md:py-28 px-4 md:px-7 lg:px-10 pt-6 md:pt-[50px] pb-14 md:pb-[103px]">
@@ -33,32 +35,57 @@ export default function ConnectWithTopExpertsSection() {
             </div>
             <div className="flex flex-col md:flex-row gap-14 md:gap-[108px] items-center mt-14 md:mt-[104px]">
                 <div className="max-w-[515px]">
-                    {
-                        selected === 'Find an Expert' ? (
-                            <>
-                                <h6 className="font-semibold text-[32px]/10 tracking-[-1%]">Discover your go-to guru!</h6>
-                                <p className="mt-4 text-[#7E8492] dark:text-[#BFC8D1]">
-                                    Looking for expert advice? Whether it’s career growth, business strategies, or personal development, MeetXO connects you with top professionals worldwide. Browse through experts in various fields and find the perfect match for your needs.
-
-                                </p>
-                            </>
-                        ) : selected === 'Book a Video Call' ? (
-                                <>
-                                    <h6 className="font-semibold text-[32px]/10 tracking-[-1%]"> Let&apos;s Chat!</h6>
-                                    <p className="mt-4 text-[#7E8492] dark:text-[#BFC8D1]">
-Got questions? Get real-time answers from industry experts through a quick and easy video call. No long email threads, no confusion—just instant, valuable insights from the right person at the right time.
-                                    </p>
-                                </>     
-                            ) : (
-                                    <>
-                                        <h6 className="font-semibold text-[32px]/10 tracking-[-1%]">Virtual Consultation</h6>
-                                        <p className="mt-4 text-[#7E8492] dark:text-[#BFC8D1]">
-                                            Skip the hassle of in-person meetings! Book a virtual consultation with top experts from around the world and get the advice you need—all from the comfort of your couch (or wherever you like to think best
-
-                                        </p>
-                                    </>
-                        )
-                    }
+                      {selected === "Find an Expert" ? (
+            <>
+              <h6 className="font-semibold text-[32px]/10 tracking-[-1%]">
+                Discover your go-to guru!
+              </h6>
+              <p className="mt-4 text-[#7E8492] dark:text-[#BFC8D1]">
+                Looking for expert advice? Whether it’s career growth, business
+                strategies, or personal development, MeetXO connects you with
+                top professionals worldwide. Browse through experts in various
+                fields and find the perfect match for your needs.
+              </p>
+            </>
+          ) : selected === "Book a Video Call" ? (
+            <>
+              <h6 className="font-semibold text-[32px]/10 tracking-[-1%]">
+                {" "}
+                Let&apos;s Chat!
+              </h6>
+              <p className="mt-4 text-[#7E8492] dark:text-[#BFC8D1]">
+                Got questions? Get real-time answers from industry experts
+                through a quick and easy video call. No long email threads, no
+                confusion—just instant, valuable insights from the right person
+                at the right time.
+              </p>
+            </>
+          ) : selected === "Virtual Consultation" ? (
+            <>
+              <h6 className="font-semibold text-[32px]/10 tracking-[-1%]">
+                Virtual Consultation
+              </h6>
+              <p className="mt-4 text-[#7E8492] dark:text-[#BFC8D1]">
+                Skip the hassle of in-person meetings! Book a virtual
+                consultation with top experts from around the world and get the
+                advice you need—all from the comfort of your couch (or wherever
+                you like to think best
+              </p>
+            </>
+          ) : (
+            <>
+              <h6 className="font-semibold text-[32px]/10 tracking-[-1%]">
+                Offline Meeting
+              </h6>
+              <p className="mt-4 text-[#7E8492] dark:text-[#BFC8D1]">
+                MeetXO offers Offline Meetings, enabling users to connect in
+                person for meaningful discussions, consultations, and
+                networking. This feature is perfect for professionals, mentors,
+                and businesses that prefer face-to-face interactions over
+                virtual meetings.
+              </p>
+            </>
+          )}
                 </div>
                 <Image src={'/images/connect-section.png'} alt="" width={584} height={366} />
             </div>
