e7e9b1d09e202643f8a61d546666a8fe061b1ce3
goformeet <info@goformeet.co>
2025-03-04 00:29:14 +0530

    Meta data page.tsx

--- src/app/(main)/(with-sidebar)/[id]/booking/page.tsx
+++ src/app/(main)/(with-sidebar)/[id]/booking/page.tsx
@@ -1,5 +1,29 @@
 import MeetBooking from "@/components/booking/meet-booking";
+import { Metadata } from "next";
 
+
+
+export async function generateMetadata({ params }: {params: Params}): Promise<Metadata> {
+  const id = decodeURIComponent((await params).id);
+
+
+
+  return {
+    title: `${id}`,
+    description: "Connect with the world's most in-demand experts for 1-on-1 guidance. Ask questions, get expert advice, and grow with personalized insights.",
+    metadataBase: new URL("https://meetxo.ai"),
+    openGraph: {
+      title: `${id}`,
+      description: "Connect with the world's most in-demand experts for 1-on-1 guidance. Ask questions, get expert advice, and grow with personalized insights.",,
+      images: [{
+        url: "/og_image.png",
+        width: 1200,
+        height: 627,
+        alt: "MeetXO Logo",
+      }],
+    },
+  };
+}
 
 export default function Page()
 {
