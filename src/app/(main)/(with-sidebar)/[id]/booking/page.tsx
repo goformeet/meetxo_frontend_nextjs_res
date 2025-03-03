1b56af35e6d3d00919f413f9ca03e87d9b38e685
goformeet <info@goformeet.co>
2025-03-04 00:47:18 +0530

    Update page.tsx

--- src/app/(main)/(with-sidebar)/[id]/booking/page.tsx
+++ src/app/(main)/(with-sidebar)/[id]/booking/page.tsx
@@ -13,11 +13,11 @@ export async function generateMetadata({ params }: {params: Params}): Promise<Me
   const ogImageUrl = `https://${process.env.AWS_S3_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/${ogKey}`;
 
   return {
-    title: `${id}`,
+    title: `Connect with the world's most in-demand experts for 1-on-1 guidance.`,
     description: "Connect with the world's most in-demand experts for 1-on-1 guidance. Ask questions, get expert advice, and grow with personalized insights.",
     metadataBase: new URL("https://meetxo.ai"),
     openGraph: {
-      title: `${id}`,
+      title: `Connect with the world's most in-demand experts for 1-on-1 guidance.`,
       description: "Connect with the world's most in-demand experts for 1-on-1 guidance. Ask questions, get expert advice, and grow with personalized insights.",
       images: [{
         url: ogImageUrl,
