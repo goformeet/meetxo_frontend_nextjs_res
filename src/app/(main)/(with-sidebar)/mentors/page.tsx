c1dd6aeb184b42b9d979f975127ee7060d085434
goformeet <info@goformeet.co>
2025-03-07 22:55:44 +0530

    mentors page.tsx updated

--- src/app/(main)/(with-sidebar)/mentors/page.tsx
+++ src/app/(main)/(with-sidebar)/mentors/page.tsx
@@ -1,21 +1,20 @@
-import { Input } from '@/components/ui/input'
-import Image from 'next/image'
-import React from 'react'
 
-export default function Page() {
 
-    return (
-        <div className='pl-5 py-5 pr-[35px] max-w-[calc(100%-105px)]'>
-            <h1 className='text-[22px]/[28px] font-bold'>Mentors</h1>
-            <div className='mt-6 flex py-[1px] px-6 items-center rounded-[38px] border border-[#F1F2F4]'>
-                <Image src={'/icons/search-primary.svg'} alt='Search Icon' width={18} height={18} />
-                <Input type="text" placeholder="Search by name, company, role" className="border-none focus-visible:ring-0 h-fit py-4 shadow-none placeholder:text-muted-foreground" />
-            </div>
-            <div className='my-8 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 lg:gap-x-5 md:gap-y-8'>
-                {/* {professionals.map((prof) => (
-                    <ExpertCard key={prof.id} prof={prof} />
-                ))} */}
-            </div>
-        </div>
-    )
+import ExpertsList from "@/components/experts/expert-catogory-list";
+import { Hosts } from "@/services/api";
+
+const CATEGORY_NAME = "Mentors";
+const PROFESSION_ID = "678b8dd486062ddce62be676";
+
+export default async function Page() {
+  let experts = [];
+
+  try {
+    const response = await Hosts({ profession_id: PROFESSION_ID });
+    experts = response.hosts?.hosts || [];
+  } catch (error) {
+    console.error(`Error fetching ${CATEGORY_NAME}:`, error);
+  }
+
+  return <ExpertsList experts={experts} category={CATEGORY_NAME} />;
 }
