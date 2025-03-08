b48214baf8fa573833e0680e481b2b2d94bbb8b2
goformeet <info@goformeet.co>
2025-03-08 22:01:40 +0530

    Categories page updated

--- src/app/(main)/categories/page.tsx
+++ src/app/(main)/categories/page.tsx
@@ -1,47 +1,60 @@
-import MeetBooking from "@/components/booking/meet-booking";
-import { Metadata } from "next";
+import { Professions } from "@/services/api";
+import { notFound } from "next/navigation";
+import CategoryCard from "@/components/category-card";
 
-type Params = Promise<{ id: string }>;
+type Category = {
+  _id: string;
+  title: string;
+  image: string;
+  description: string;
+};
 
-export async function generateMetadata({
-  params,
-}: {
-  params: Params;
-}): Promise<Metadata> {
-  const id = decodeURIComponent((await params).id);
-
-  const ogKey = `og_images/${id}.png`;
-  const ogImageUrl = `https://${process.env.AWS_S3_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/${ogKey}`;
-
-  return {
-    title:
-      "Connect with the world's most in-demand experts for 1-on-1 guidance",
+export const metadata = {
+  title: "Expert Categories – Find Top Experts on MeetXO",
+  description:
+    "Discover a diverse range of experts on MeetXO. Connect with professionals across various fields for insightful and productive meetings.",
+  keywords:
+    "MeetXO experts, expert categories, professional advice, business experts, tech experts, coaching, mentorship, industry specialists, expert consultations, skill development",
+  metadataBase: new URL("https://meetxo.ai"),
+  openGraph: {
+    title: "Expert Categories – Find Top Experts on MeetXO",
     description:
-      "Connect with the world's most in-demand experts for 1-on-1 guidance. Ask questions, get expert advice, and grow with personalized insights.",
-    metadataBase: new URL("https://meetxo.ai"),
-    openGraph: {
-      title:
-        "Connect with the world's most in-demand experts for 1-on-1 guidance",
-      description:
-        "Connect with the world's most in-demand experts for 1-on-1 guidance. Ask questions, get expert advice, and grow with personalized insights.",
-      images: [
-        {
-          url: ogImageUrl,
-          width: 1200,
-          height: 627,
-          alt: "MeetXO Logo",
-        },
-      ],
-    },
-  };
-}
+      "Discover a diverse range of experts on MeetXO. Connect with professionals across various fields for insightful and productive meetings.",
+    url: "https://meetxo.ai/categories",
+    images: [
+      {
+        url: "/og_image.png",
+        width: 1200,
+        height: 630,
+        alt: "MeetXO Experts",
+      },
+    ],
+  },
+};
 
-export default function Page() {
+export default async function Page() {
+  const data = await Professions();
+
+  const professions = data["professions"];
+
+  if (!professions || !professions.length) {
+    console.log(professions);
+    notFound();
+  }
+
   return (
-    <>
-      <div className="px-4 md:px-7 lg:px-10 w-full relative flex flex-col justify-between">
-        <MeetBooking />
+    <main className="px-4 md:px-7 lg:px-10 pb-20 pt-10">
+      <div className="flex justify-between items-center">
+        <h1 className="text-lg/5 md:text-[22px]/[28px] font-bold ">
+          Expert Categories - Discover a diverse range of experts on MeetXO
+        </h1>
       </div>
-    </>
+
+      <div className="mt-5 grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 lg:gap-5">
+        {professions.map((category: Category) => (
+          <CategoryCard key={category._id} category={category} />
+        ))}
+      </div>
+    </main>
   );
 }
